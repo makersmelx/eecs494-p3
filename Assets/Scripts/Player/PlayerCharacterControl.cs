1c97cab4318686a18a84bb85117976a7fb6fdee0
Jiayao Wu <makersmelx@gmail.com>
2021-11-05 17:22:51 -0400

    feat(#26): when the character is faced to the wall and close enough and he is jumping in the air, he will rotate to stand on the wall and able to jump on the wall

--- Assets/Scripts/Player/PlayerCharacterControl.cs
+++ Assets/Scripts/Player/PlayerCharacterControl.cs
@@ -6,8 +6,14 @@ using UnityEngine;
 [RequireComponent(typeof(PlayerInputHandler), typeof(CharacterController))]
 public class PlayerCharacterControl : MonoBehaviour
 {
-    [Header("References")] [Tooltip("Reference to the main camera used for the player")]
+    [Header("General")] [Tooltip("Force applied downward when in the air")]
+    public float gravityDownForce = 20f;
+
+    [Header("Camera")] [Tooltip("Reference to the main camera used for the player")]
     public Camera playerCamera;
+
+    [Tooltip("Rotation speed for moving the camera")]
+    public float cameraMoveSpeed = 200f;
 
     [Header("Movement")] [Tooltip("Max movement speed when grounded")]
     public float maxSpeedOnGround = 10f;
@@ -15,29 +21,30 @@ public class PlayerCharacterControl : MonoBehaviour
     [Tooltip("How fast the player can change his speed")]
     public float speedSharpnessOnGround = 15;
 
-    [Header("General")] [Tooltip("Force applied downward when in the air")]
-    public float gravityDownForce = 20f;
-
     [Header("Jump")] [Tooltip("Force applied upward when jumping")]
     public float jumpForce = 9f;
 
     [Header("Check Grounded")] [Tooltip("Physic layers checked to consider the player grounded")]
     public LayerMask groundCheckLayers = -1;
 
-    [Header("Check Grounded")]
     [Tooltip("Distance from the bottom of the character controller capsule to check grounded")]
     public float groundCheckDistance = 1f;
 
-    [Header("Check Grounded")]
     [Tooltip("Distance from the bottom of the character controller capsule to check on ground in the air")]
     public float groundCheckDistanceInAir = 0.07f;
 
-    [Header("Check Grounded")] [Tooltip("The waiting time for the next ground detection since last jump")]
+    [Tooltip("The waiting time for the next ground detection since last jump")]
     public float checkGroundedCooldownTime = 0.2f;
 
-    [Header("Camera")] [Tooltip("Rotation speed for moving the camera")]
-    public float cameraMoveSpeed = 200f;
+    [Header("Wall Walk")] [Tooltip("The component from this game object's child that climbs the walls around")]
+    public LayerMask wallCheckLayers = -1;
 
+    [Tooltip(
+        "(Ratio to the character's radius) the max distance from the position to detect the wall")]
+    public float wallDetectionMaxDetectionDistanceRatioForward = 0.5f;
+
+    [Tooltip("(Ratio to the character's radius) the radius of the wall detection sphere")]
+    public float wallDetectionSphereRadiusRatio = 0.6f;
 
     public enum CharacterState
     {
@@ -46,44 +53,69 @@ public class PlayerCharacterControl : MonoBehaviour
         WallWalk = 2,
     }
 
+    // Non Editable Fields
+
     // todo: make it not editable
     public CharacterState currentState;
 
     public Vector3 CharacterVelocity { get; set; }
 
-    /*
-     * The coefficient of the camera speed, may be affected by aiming or other actions
-     * todo: if aiming is needed, modify here
-     */
+    // if the character is close to a wall, he can climb onto the wall
+    // this value will be modified externally
+    public bool CanJumpOntoWall { get; set; }
+
+    public static PlayerCharacterControl Instance;
+
+    // todo: if aiming is needed, modify here
+    // The coefficient of the camera speed, may be affected by aiming or other actions
     public float CameraCoefficient
     {
         get { return 1f; }
     }
 
-    /*
-     * Component Reference
-     */
+    // Component Reference
     private PlayerInputHandler playerInputHandler;
     private CharacterController characterController;
 
-    /*
-     * Runtime Value
-     */
+    // Runtime Value
     private float currentCameraAngleVertical = 0f;
     private float lastJumpTime = 0f;
-    private Vector3 currentGroundNormal;
+    [SerializeField] private Vector3 currentGroundNormal;
+
+    private Vector3 wallDetectionCastOrigin => transform.position;
+
+    private float wallDetectionMaxDistance =>
+        characterController.radius * wallDetectionMaxDetectionDistanceRatioForward;
+
+    private float wallDetectionSphereRadius => characterController.radius * wallDetectionSphereRadiusRatio;
 
     private void Start()
     {
         playerInputHandler = GetComponent<PlayerInputHandler>();
         characterController = GetComponent<CharacterController>();
+        currentGroundNormal = Vector3.up;
     }
 
     private void Update()
     {
         HandleCameraMove();
+
         CheckGrounded();
+
+        HandleCharacterJumpOntoWall();
         HandleCharacterMove();
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void HandleCameraMove()
@@ -108,21 +140,13 @@ public class PlayerCharacterControl : MonoBehaviour
 
     private void CheckGrounded()
     {
-        // Skip ground check when the character is walking on the wall
-        if (currentState == CharacterState.WallWalk)
-        {
-            return;
-        }
-
         float finalCheckDistance =
-            currentState == CharacterState.DefaultGrounded
-                ? groundCheckDistance + characterController.skinWidth
-                : groundCheckDistanceInAir;
+            currentState == CharacterState.InAir
+                ? groundCheckDistanceInAir
+                : groundCheckDistance + characterController.skinWidth;
 
         // reset
-        currentGroundNormal = Vector3.up;
-
-        Vector3 toGroundDirection = Vector3.down;
+        Vector3 toGroundDirection = -1f * currentGroundNormal;
         // only try to detect ground if it's been a short amount of time since last jump;
         if (Time.time >= lastJumpTime + checkGroundedCooldownTime)
         {
@@ -131,7 +155,9 @@ public class PlayerCharacterControl : MonoBehaviour
                 origin,
                 toGroundDirection,
                 out RaycastHit hit,
-                finalCheckDistance
+                finalCheckDistance,
+                groundCheckLayers,
+                QueryTriggerInteraction.Ignore
             ))
             {
                 currentGroundNormal = hit.normal;
@@ -162,13 +188,19 @@ public class PlayerCharacterControl : MonoBehaviour
             CharacterVelocity =
                 Vector3.Lerp(CharacterVelocity, targetVelocity, speedSharpnessOnGround * Time.deltaTime);
             // Jumping
-            if (playerInputHandler.GetJumpInputIsHolding())
             {
-                // todo: if we need to clear the up vector of the velocity
-                CharacterVelocity += transform.up * jumpForce;
-                currentState = CharacterState.InAir;
-                currentGroundNormal = Vector3.up;
-                lastJumpTime = Time.time;
+                if (playerInputHandler.GetJumpInputIsHolding())
+                {
+                    // todo: if we need to clear the up vector of the velocity
+                    CharacterVelocity += currentGroundNormal * jumpForce;
+                    currentState = CharacterState.InAir;
+                    currentGroundNormal = Vector3.up;
+                    // todo: add coroutine to make it not so dizzy
+                    Vector3 forwardHorizontal = transform.forward;
+                    forwardHorizontal.y = 0;
+                    transform.rotation = Quaternion.LookRotation(forwardHorizontal, Vector3.up);
+                    lastJumpTime = Time.time;
+                }
             }
         }
         else if (currentState == CharacterState.InAir)
@@ -179,7 +211,72 @@ public class PlayerCharacterControl : MonoBehaviour
                 Vector3.Lerp(CharacterVelocity, targetVelocity, speedSharpnessOnGround * Time.deltaTime);
             CharacterVelocity += Vector3.down * gravityDownForce * Time.deltaTime;
         }
-        
+
         characterController.Move(CharacterVelocity * Time.deltaTime);
+    }
+
+    private void HandleCharacterJumpOntoWall()
+    {
+        if (currentState == CharacterState.InAir
+            && Physics.SphereCast(
+                wallDetectionCastOrigin,
+                wallDetectionSphereRadius,
+                transform.forward,
+                out RaycastHit hit,
+                wallDetectionMaxDistance,
+                wallCheckLayers,
+                QueryTriggerInteraction.Ignore))
+        {
+            currentState = CharacterState.WallWalk;
+
+            // todo: add coroutine to make it not so dizzy
+            // When climbing the wall, the character will rotate during which the camera will always look at the forward direction
+
+            // A vector is a forward direction if it is a normal vector
+            // and current ground normal, wall normal and this vector are intersectional with each other
+
+            // This value is the forward direction that will not change when climbing to the wall
+            Vector3 forwardHorizontal = GetForwardVectorClimbingWall(currentGroundNormal, hit.normal);
+
+            transform.rotation = Quaternion.LookRotation(forwardHorizontal, hit.normal);
+            currentGroundNormal = hit.normal;
+            transform.position = hit.point;
+
+            CharacterVelocity -= currentGroundNormal * jumpForce;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(
+            wallDetectionCastOrigin + transform.forward * wallDetectionMaxDistance,
+            wallDetectionSphereRadius);
+    }
+
+    private Vector3 GetForwardVectorAfterWallWalkRotation(Vector3 forward, Vector3 wallNormal, Vector3 destPoint)
+    {
+        float cameraHeight = 1f;
+        wallNormal = wallNormal.normalized;
+        forward = forward.normalized;
+        Vector3 currentCamera = transform.position + transform.up * cameraHeight;
+        Vector3 futureCamera = destPoint + wallNormal * cameraHeight;
+        Vector3 BAProjection = Vector3.Dot((futureCamera - currentCamera), wallNormal) * wallNormal;
+        float cosine = Vector3.Dot(forward, wallNormal);
+        Vector3 viewEndPoint = currentCamera + forward * BAProjection.magnitude / cosine;
+        return (viewEndPoint - futureCamera).normalized;
+    }
+
+    private Vector3 GetForwardVectorClimbingWall(Vector3 currentNormal, Vector3 nextNormal)
+    {
+        Vector3 forwardHorizontal = Vector3.Cross(currentNormal, nextNormal);
+
+        // If the player is facing back, reverse this value
+        if (Vector3.Dot(forwardHorizontal, transform.forward) < 0)
+        {
+            forwardHorizontal *= -1;
+        }
+
+        return forwardHorizontal;
     }
 }
