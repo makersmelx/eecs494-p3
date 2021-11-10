7dcbae91bc61fe2a729d70c9b5c284b82a4ca2d2
Jiayao Wu <makersmelx@gmail.com>
2021-11-09 20:07:38 -0500

    refact(#36): make the new player prefab work

--- Assets/Scripts/Player/PlayerMoveControl.cs
+++ Assets/Scripts/Player/PlayerMoveControl.cs
@@ -36,23 +36,13 @@ public class PlayerMoveControl : MonoBehaviour
 
 
     // ============================================= Check Grounded =============================================
-    [Header("Check Grounded")] [Tooltip("Physic layers checked to consider the player grounded")]
-    public LayerMask groundCheckLayers = -1;
-
-    [Tooltip("Distance from the bottom of the character controller capsule to check grounded")]
-    public float groundCheckDistance = 1f;
-
-    [Tooltip("Distance from the bottom of the character controller capsule to check on ground in the air")]
-    public float groundCheckDistanceInAir = 0.07f;
-
-    [Tooltip("The waiting time for the next ground detection since last jump")]
-    public float checkGroundedCooldownTime = 0.2f;
+    [Header("Check Grounded")]  
+    public ObstructionDetection groundDetection;
 
 
     // ============================================= Component Reference ============================================= 
     private PlayerInputHandler playerInputHandler;
     private Rigidbody rigidBody;
-    private CapsuleCollider capsuleCollider;
     public static PlayerMoveControl Instance;
 
 
@@ -63,34 +53,16 @@ public class PlayerMoveControl : MonoBehaviour
         get { return 1f; }
     }
 
-    public enum CharacterState
-    {
-        InAir = 0,
-        DefaultGrounded = 1,
-        WallRun = 2,
-        OnLedge = 3,
-    }
-
 
     // ============================================= Runtime Value ============================================= 
     // todo: make it not editable
-    [Header("Runtime Value for Display")] public CharacterState currentState;
 
     private float currentCameraAngleVertical = 0f;
-    private float lastJumpTime = 0f;
-
-    private Vector3 currentGroundNormal;
-
-    private Vector3 initialCameraOffset;
-    private Vector3 cameraOffset = Vector3.zero;
 
     // todo (#33): this is only a temp solution for triggering winning, an issue is created to modify this, check #33 for details
     public bool isWin = false;
 
-    private bool isJumping = false;
-
-    // A count set to prevent multiple ledge climb check when the character is trying to leave the ledge
-    private int ledgeJudgeCount = 0;
+    private bool isGrounded = false;
 
     private void Awake()
     {
@@ -108,9 +80,6 @@ public class PlayerMoveControl : MonoBehaviour
     {
         playerInputHandler = GetComponent<PlayerInputHandler>();
         rigidBody = GetComponent<Rigidbody>();
-        capsuleCollider = GetComponent<CapsuleCollider>();
-        currentGroundNormal = Vector3.up;
-        initialCameraOffset = playerCamera.transform.position - transform.position;
     }
 
     private void Update()
@@ -155,42 +124,7 @@ public class PlayerMoveControl : MonoBehaviour
 
     private void CheckGrounded()
     {
-        float finalCheckDistance =
-            currentState == CharacterState.InAir
-                ? groundCheckDistanceInAir
-                : groundCheckDistance;
-
-        // reset
-        Vector3 toGroundDirection = -currentGroundNormal;
-        // only try to detect ground if it's been a short amount of time since last jump;
-        if (Time.time >= lastJumpTime + checkGroundedCooldownTime)
-        {
-            Vector3 origin = transform.position + toGroundDirection * capsuleCollider.height / 2;
-            bool isGrounded = Physics.Raycast(
-                origin,
-                toGroundDirection,
-                out RaycastHit hit,
-                finalCheckDistance
-            );
-            Debug.DrawLine(origin, origin + toGroundDirection * finalCheckDistance, Color.red);
-            // True if the user is on the ground or on a wall
-            if (isGrounded)
-            {
-                // Only consider this a valid ground hit if the ground normal goes in the same direction as the character up
-                // and if the slope angle is lower than the character controller's limit
-                if (Vector3.Dot(hit.normal, toGroundDirection * -1) > 0f)
-                {
-                    if (currentState == CharacterState.InAir)
-                    {
-                        currentState = CharacterState.DefaultGrounded;
-                    }
-                }
-            }
-            else
-            {
-                currentState = CharacterState.InAir;
-            }
-        }
+        isGrounded = groundDetection.isObstructed;
     }
 
     private void HandleCharacterMove()
@@ -203,7 +137,7 @@ public class PlayerMoveControl : MonoBehaviour
 
         // Clamp the horizontal speed
         {
-            Vector3 upVelocity = Vector3.Dot(rigidBody.velocity, transform.up) * currentGroundNormal;
+            Vector3 upVelocity = Vector3.Dot(rigidBody.velocity, transform.up) * Vector3.up;
             Vector3 horizonVelocity = rigidBody.velocity - upVelocity;
             rigidBody.velocity = Vector3.ClampMagnitude(horizonVelocity, maxSpeedOnGround) + upVelocity;
             rigidBody.AddForce(targetVelocity * Time.deltaTime);
@@ -211,7 +145,7 @@ public class PlayerMoveControl : MonoBehaviour
 
         // Adjust the speed to zero when there is no input and the character is not in the air
         {
-            if (currentState == CharacterState.DefaultGrounded)
+            if (isGrounded)
             {
                 float forwardSpeed = Vector3.Dot(rigidBody.velocity, transform.forward);
                 float rightSpeed = Vector3.Dot(rigidBody.velocity, transform.right);
@@ -233,19 +167,11 @@ public class PlayerMoveControl : MonoBehaviour
     // Jump Function will not check the character's current state
     private void HandleCharacterJump()
     {
-        if (currentState != CharacterState.InAir)
+        if (isGrounded)
         {
-            if (!isJumping && playerInputHandler.GetJumpInputIsHolding())
+            if (playerInputHandler.GetJumpInputIsHolding())
             {
-                rigidBody.AddForce(jumpForce * currentGroundNormal);
-                currentState = CharacterState.InAir;
-                currentGroundNormal = Vector3.up;
-                lastJumpTime = Time.time;
-                isJumping = true;
-            }
-            else
-            {
-                isJumping = false;
+                rigidBody.AddForce(jumpForce * Vector3.up);
             }
         }
     }
