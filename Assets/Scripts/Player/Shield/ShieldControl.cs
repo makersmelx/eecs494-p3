71e2c89ff59dce1458cb8bd81152d231053cdddb
Jiayao Wu <makersmelx@gmail.com>
2021-11-13 01:25:31 -0500

    feat(#40): integrate with time manager

--- Assets/Scripts/Player/Shield/ShieldControl.cs
+++ Assets/Scripts/Player/Shield/ShieldControl.cs
@@ -7,13 +7,17 @@ using UnityEngine;
 public class ShieldControl : MonoBehaviour
 {
     public float shieldMaxSpeedThreshold = 0.8f;
+
+    [Tooltip("Time Bonus when the shield destroys a single bullet")]
+    public float defendBonusTime = 1f;
+
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(GameConstants.BulletTag))
         {
             Destroy(other.gameObject);
-            // todo: the function to modify the player's time
+            TimeManager.Instance.AddTime(defendBonusTime);
         }
     }
 }
