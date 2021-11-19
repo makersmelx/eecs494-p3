6f2a8e530f536978faa32fb4789a9990a79a8c97
Bohan Xu <xubohan@umich.edu>
2021-11-20 07:53:02 +0800

    Upload

--- Assets/Scripts/Player/PlayerBulletDetection.cs
+++ Assets/Scripts/Player/PlayerBulletDetection.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class PlayerBulletDetection : MonoBehaviour
 {
-    [SerializeField] float bulletHitTimeReduction = 10f;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag(GameConstants.BulletTag))
         {
-            TimeManager.Instance.ReduceTime(bulletHitTimeReduction);
+            Bullet bullet = collision.gameObject.GetComponent<Bullet>();
+            TimeManager.Instance.ReduceTime(bullet.timeReductionOnHit) ;
         }
     }
 }
