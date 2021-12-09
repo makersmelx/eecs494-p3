8a77b90a15a14a4e21f30530a58ea412b502c2ae
Jiayao Wu <makersmelx@gmail.com>
2021-12-08 21:41:42 -0500

    feat(#90): stop time can affect bullet

--- Assets/Scripts/Enemy/Bullet.cs
+++ Assets/Scripts/Enemy/Bullet.cs
@@ -8,21 +8,27 @@ public class Bullet : MonoBehaviour
     // -------------------------------------------------------------------------
     // Configurable params
     // -------------------------------------------------------------------------
-    [Header("Bullet configuration")]
-    [SerializeField] float velocity = 5f;
+    [Header("Bullet configuration")] [SerializeField]
+    float velocity = 5f;
+
     [SerializeField] float maxLifetimeInSeconds = 10f;
     [SerializeField] AudioClip bulletFlyingSound;
     public float timeReductionOnHit = 4f;
     public float timeIncreaseOnHit = 4f;
-    [Header("Accelaertaion configuration")]
-    [SerializeField] bool isAccelerate = true;
+
+    [Header("Accelaertaion configuration")] [SerializeField]
+    bool isAccelerate = true;
+
     [SerializeField] float accelerationParameter = 2.5f;
+
     [SerializeField] float maxSpeed = 100f;
+
     // -------------------------------------------------------------------------
     // Internal State
     // -------------------------------------------------------------------------
     private float timeOfInstantiation;
     private AudioSource audioSource;
+    private float timer;
 
     // -------------------------------------------------------------------------
     // Public methods
@@ -44,26 +50,28 @@ public class Bullet : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
         audioSource.clip = bulletFlyingSound;
         audioSource.Play();
+        timer = Time.time;
     }
 
     void Update()
     {
+        timer += Time.deltaTime * LevelManager.Instance.timeScale;
         // Destroy if alive for too long
-        if (Time.time - timeOfInstantiation > maxLifetimeInSeconds)
+        if (timer - timeOfInstantiation > maxLifetimeInSeconds)
         {
             Destroy(gameObject);
             return;
         }
 
         // Update position
-        transform.position += transform.forward * (Time.deltaTime * velocity);
+        transform.position += transform.forward * (Time.deltaTime * LevelManager.Instance.timeScale * velocity);
 
         if (isAccelerate && velocity < maxSpeed) Accelerate();
     }
 
     void Accelerate()
     {
-        velocity += accelerationParameter * Time.deltaTime;
+        velocity += accelerationParameter * Time.deltaTime * LevelManager.Instance.timeScale;
     }
 
     private void OnCollisionEnter(Collision collision)
